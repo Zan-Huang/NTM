5e30e6ac31bc5cd0e773e785169733d145f84b27
zankner <zackankner@gmail.com>
2019-01-17 15:56:50 -0500

    idk

--- NTMCode/read.py
+++ NTMCode/read.py
@@ -12,8 +12,8 @@ b = tf.get_variable("memory_matrix_test", initializer = init2)
 '''
 def reading_function(weight_vector, memory_matrix):
     if weight_vector.shape.as_list()[0] != memory_matrix.shape.as_list()[0]:
-        raise Exception('The size of the memory matrix does not match the memory vector. Check size of W and make sure it is N')
-    read_vector = tf.tensordot(weight_vector, memory_matrix,1)
+        raise Exception('The size of the memory matrix does not match the memory matrix. Check size of W and make sure it is N')
+    read_vector = tf.tensordot(weight_vector, memory_matrix, 1)
     return read_vector
 '''
 with tf.Session() as sess:
