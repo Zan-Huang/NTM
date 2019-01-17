ba31e2ba1e301863b7ea537aadc8f46373b37466
zankner <zackankner@gmail.com>
2019-01-17 01:08:21 -0500

    added add

--- NTMCode/write.py
+++ NTMCode/write.py
@@ -19,7 +19,7 @@ def erase_function(weight_vector, erase_vector, past_timestep_memory_matrix):
 	if weight_vector.shape.as_list()[0] != past_timestep_memory_matrix.shape.as_list()[0]:
 		raise Exception('The size of the memory matrix does not match the memory vector. Check size of W and make sure it is N')
 	if erase_vector.shape.as_list()[0] != past_timestep_memory_matrix.shape.as_list()[1]:
-		raise Exception('The size of the weight vector does not match the erase vector. Check size of W and make sure it is E')
+		raise Exception('The size of the weight vector does not match the erase vector. Check size of E and make sure it is M')
 	ones = tf.ones(erase_vector.shape, tf.float64)
 	erased = []
 	for i in range(0,past_timestep_memory_matrix.shape.as_list()[0]):
@@ -27,7 +27,22 @@ def erase_function(weight_vector, erase_vector, past_timestep_memory_matrix):
 		outers = tf.multiply(past_timestep_memory_matrix[i], inners)
 		erased.append(outers)
 	erased_memory = tf.stack([erase for erase in erased])
-	return erased_memory
+	return erased_memory, weight_vector
+
+def write_function(weight_vector, add_vector, erased_matrix):
+	add_vector = add_vector[0]
+	if weight_vector.shape.as_list()[0] != erased_matrix.shape.as_list()[0]:
+		raise Exception('The size of the memory matrix does not match the memory vector. Check size of W and make sure it is N')
+	if add_vector.shape.as_list()[0] != erased_matrix.shape.as_list()[1]:
+		raise Exception('The size of the add vector does not match the erased matrix. Check size of A and make sure it is M')
+	composite = []
+	for i in range(0,erased_matrix.shape.as_list()[0]):
+		weighted_add = tf.multiply(weight_vector[i],add_vector)
+		erased_added = tf.add(erased_matrix[i], weighted_add)
+		composite.append(erased_added)
+	composite_memory = tf.stack([comp for comp in composite])
+	return composite_memory
 
 with tf.Session() as sess:
-	print(erase_function(a,c,b))
+	erased, weights = erase_function(a,c,b)
+	print(write_function(weights, c, erased))
