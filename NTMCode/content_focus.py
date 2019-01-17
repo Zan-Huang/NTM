ac47dc433ea254c4acc64a250280d3d826599edf
Zan Huang <zan@zans-mbp.milton.edu>
2019-01-17 13:45:29 -0500

    Finished content focus without the testing

--- NTMCode/content_focus.py
+++ NTMCode/content_focus.py
@@ -13,5 +13,20 @@ def similarity_measure(key_vector, memory_vector_slice):
     return similarity_measure_result
 
 
-def content_address(beta_strength, similarity_measure):
-    content_vector_top = tf.math.exp(tf.multiply(beta_strength, similarity_measure))
+def content_address(beta_strength, key_vector, memory_vector):
+    content_filler = tf.constant(np.zeros(memory_vector.shape.as_list()[0]))
+    weight_vector = tf.get_variable("content_weights", intializer = content_filler)
+    composite_top = []
+
+    content_bottom = 0
+    for j in range(0, memory_vector.shape.as_list()[0]):
+        content_bot_temp = tf.math.exp(tf.multiply(beta_strength, similarity_measure(key_vector, memory_vector[j])))
+        content_bottom = tf.add(content_bottom_temp, content_bottom)
+
+    for i in range(0, memory_vector.shape.as_list()[0]):
+        content_vector_top = tf.math.exp(tf.multiply(beta_strength, similarity_measure(key_vector, memory_vector[i])))
+        final_top = content_vector_top / content_bottom
+        composite_top.append(final_top)
+
+    final_focus_vector = tf.stack([piece for piece in composite_top])
+    return final_focus_vector
