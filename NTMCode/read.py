a32a18a4c6a391c7992c9c333753af2e1fd59d5f
zankner <zackankner@gmail.com>
2019-01-17 00:06:37 -0500

    reset

--- NTMCode/read.py
+++ NTMCode/read.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 
 #Below is Testing
-
+"""
 init_op = tf.global_variables_initializer()
 
 init = tf.constant(np.random.rand(1, 10))
@@ -11,15 +11,14 @@ init2 = tf.constant(np.random.rand(10, 20))
 
 a = tf.get_variable("weight_vector_test", initializer = init)
 b = tf.get_variable("memory_vector_test", initializer = init2)
-
+"""
 def reading_function(weight_vector, memory_vector):
     if weight_vector.shape.as_list()[1] != memory_vector.shape.as_list()[0]:
         raise Exception('The size of the memory matrix does not match the memory vector. Check size of W and make sure it is N')
     read_vector = tf.matmul(weight_vector, memory_vector)
-    print(read_vector)
     return read_vector
-
+"""
 with tf.Session() as sess:
     print(reading_function(a, b))
     sess.run(init_op)
-
+"""
