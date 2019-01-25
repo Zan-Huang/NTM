479987ae6405c434da49ba89610d94067efdde09
zankner <zackankner@gmail.com>
2019-01-25 18:02:38 -0500

    fixed testing

--- NTMCode/function_tests.py
+++ NTMCode/function_tests.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from __future__ import print_function
 import tensorflow as tf
 import numpy as np
@@ -39,29 +38,8 @@ def main():
     interpolation_weight = focus_location.location_lookup(w_prev, content_weight, inter_gate, one)
     blurred_weight = convshift.conv_function(interpolation_weight, conv_vec)
     weight = convshift.sharpness_function(blurred_weight,gamma)
-    return weight
+    return blurred_weight
 
 with tf.Session() as sess:
     print(main())
     sess.run(init_op)
-=======
-import tensorflow
-import content_focus
-import convshift
-import function_tests
-import read
-import weight
-import write
-
-def function_tests():
-    initTESTMemory_Matrix = tf.constant(np.random.rand(30, 20))
-    initWeightMatrix = tf.constant(np.random.rand(20, 1))
-
-    ubutTESTMemory_Matrix = tf.get_variable("memory_vector_fortesting", initializer = initTESTMemory_Matrix)
-    ubutWeightMatrix = tf.get_variable("memory_weight_test", initializer = initWeightMatrix)
-
-    return tests
-
-if __name__ == '__function_tests__':
-    function_tests()
->>>>>>> b882755514d1d8041d6128a57cb5c10a78d30c33
