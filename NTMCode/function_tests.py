b882755514d1d8041d6128a57cb5c10a78d30c33
Zan Huang <zan@Zans-MacBook-Pro.local>
2019-01-25 14:59:36 -0500

    setup done

--- NTMCode/function_tests.py
+++ NTMCode/function_tests.py
@@ -5,3 +5,15 @@ import function_tests
 import read
 import weight
 import write
+
+def function_tests():
+    initTESTMemory_Matrix = tf.constant(np.random.rand(30, 20))
+    initWeightMatrix = tf.constant(np.random.rand(20, 1))
+
+    ubutTESTMemory_Matrix = tf.get_variable("memory_vector_fortesting", initializer = initTESTMemory_Matrix)
+    ubutWeightMatrix = tf.get_variable("memory_weight_test", initializer = initWeightMatrix)
+
+    return tests
+
+if __name__ == '__function_tests__':
+    function_tests()
