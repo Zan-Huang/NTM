1517f03a4bb069ea4884584195b8d1e11886b64a
Zan Huang <zan@Zans-MBP.fios-router.home>
2019-01-26 23:29:03 -0500

    Controller initial state set

--- NTMCode/controller.py
+++ NTMCode/controller.py
@@ -19,8 +19,19 @@ class NTM(object):
     def __call__(self, x, previous_controller, prev_read):
         with self.graph_argument.name_scope("concat"):
             NTM_Input = tf.concat([x], prev_read, axis=1)
+            
+        ###Much code to fill in here on Sunday
 
-        return NTM_output, controller_state, read_vector, write_vector, memory
+        self.step += 1
+
+        return NTM_output, controller_state, read_vector_batch, weight_batch, memory
 
     def initial_state(self, batch_size, dtype):
-        
+        def expand(x, dim, N):
+            return tf.concat([tf.expand_dims(x, dim) for _ in range(N)], axis=dim)
+
+        controller_state = expand(tf.tanh(tf.get_variable('init_state', self.unit_size, initializer=tf.random_normal_initializer(mean=0.0, stddev=0.25))), dim=0, N=batch_size)
+        read_vector_batch = [expand(tf.nn.softmax(tf.get_variable('init_read', [self.memory.shape[1]], initializer=tf.random_normal_initializer(mean=0.0, stddev=0.5))), dim=0, N=batch_size)]
+        weight_batch = [expand(tf.nn.softmax(tf.get_variable('init_write', [self.memory.shape[0]], initializer=tf.random_normal_initializer(mean=0.0, stddev=0.5))), dim=0, N=batch_size)]
+        memory = expand(tf.tanh(tf.get_variable('init_memory', [self.memory.shape[0], self.memory.shape[1]], initializer=tf.random_normal_initializer(mean=0.0, stddev=0.5))), dim=0, N=batch_size)
+        return controller_state, read_vector_batch, weight_batch, memory
