0ee8ce65e53b741bc777a0e338ceabbd5b1d9bd9
Zan Huang <zan@zans-mbp.milton.edu>
2019-01-28 12:32:30 -0500

    Main file has issues

---
+++ main.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+import sys
+
+sys.path.insert(0, 'NTMCode')
+
+import function_tests
+import controller
+
+def main():
+    
+
+if __name__ == '__main__':
+    main()
