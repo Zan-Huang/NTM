59037bc50dc48eb4156a165ab2ec232296f8fd3c
Zan Huang <zan@zans-mbp.milton.edu>
2019-01-29 15:40:52 -0500

    model.py variable naming

--- model.py
+++ model.py
@@ -6,14 +6,15 @@ import numpy as np
 class StockPredictor(object):
     def __init__(self, output, seq_length, batch_size, output_dim, vector_dim):
         self.x = tf.placeholder(name='x',dtype=tf.float32,shape=[batch_size, seq_length, vector_dim])
-        self.y = output
+        self.y = label
 
         cell = NTMCell #fill here
         controller_state, read_vector, w = cell.initial_state(batch_size, tf.float32)
         for i in range(seq_length):
             output, controller_state, read_vector, w = cell.call(self.x[:,i,:],controller_state,w,read_vector)
 
-        loss = tf.losses.mean_squared_error(output, y)
+        loss = tf.losses.mean_squared_error(output, label)
+        self.output = output
 
         with tf.variable_scope('optimizer'):
             self.optimizer = tf.train.AdamOptimizer(lr, b1, b2, e)
@@ -22,5 +23,5 @@ class StockPredictor(object):
 
             self.loss_summary = tf.summary.scalar(loss)
 
-        def return_parameters():
-            return output, loss
+    def return_parameters():
+        return self.y, self.output
