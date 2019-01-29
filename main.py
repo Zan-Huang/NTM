e9635d61f4b2859b615bd3887aebc1592e88bc59
Zan Huang <zan@zans-mbp.milton.edu>
2019-01-29 15:26:19 -0500

    NTM

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import sys
+import model
 
 sys.path.insert(0, 'NTMCode')
 
@@ -8,7 +9,7 @@ import function_tests
 import controller
 
 def main():
-    
+    model1 = model.StockPredictor()
 
 if __name__ == '__main__':
     main()
