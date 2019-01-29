a8d8e9370bdbc3b659f6271a1b0918b0ddd8deff
Zan Huang <zan@zans-mbp.milton.edu>
2019-01-29 15:25:30 -0500

    Neural Turing Machine Model

--- model.py
+++ model.py
@@ -2,20 +2,25 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-def __init__(self, output, seq_length, batch_size, output_dim, vector_dim):
-    self.x = tf.placeholder(name='x',dtype=tf.float32,shape=[batch_size, seq_length, vector_dim])
-    self.y = output
 
-    cell = NTMCell #fill here
-    controller_state, read_vector, w = cell.initial_state(batch_size, tf.float32)
-    for i in range(seq_length):
-        output, controller_state, read_vector, w = cell.call(self.x[:,i,:],controller_state,w,read_vector)
+class StockPredictor(object):
+    def __init__(self, output, seq_length, batch_size, output_dim, vector_dim):
+        self.x = tf.placeholder(name='x',dtype=tf.float32,shape=[batch_size, seq_length, vector_dim])
+        self.y = output
 
-    loss = tf.losses.mean_squared_error(output,y)
+        cell = NTMCell #fill here
+        controller_state, read_vector, w = cell.initial_state(batch_size, tf.float32)
+        for i in range(seq_length):
+            output, controller_state, read_vector, w = cell.call(self.x[:,i,:],controller_state,w,read_vector)
 
-    with tf.variable_scope('optimizer'):
-        self.optimizer = tf.train.AdamOptimizer(lr, b1, b2, e)
-        grads = self.optimizer.compute_gradients(self.loss)
-        self.train_op = self.optimizer.apply_gradients(grads)
+        loss = tf.losses.mean_squared_error(output, y)
 
-    self.loss_summary = tf.summary.scalar(loss)
+        with tf.variable_scope('optimizer'):
+            self.optimizer = tf.train.AdamOptimizer(lr, b1, b2, e)
+            grads = self.optimizer.compute_gradients(self.loss)
+            self.train_op = self.optimizer.apply_gradients(grads)
+
+            self.loss_summary = tf.summary.scalar(loss)
+
+        def return_parameters():
+            return output, loss
