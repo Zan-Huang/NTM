9c363987d9b8b29aa6ed9dded2935b9d0f9e1aaa
Zan Huang <zan@zans-mbp.milton.edu>
2019-02-13 22:39:16 -0500

    Some more functions

--- DataFetch/mainarray.py
+++ DataFetch/mainarray.py
@@ -1,3 +1,14 @@
 import fetch
 import numpy as np
 import json
+import pandas as pd
+
+csv_file = "companylist.csv"
+
+csv_read = pd.read_csv(csv_file)
+print(csv_read)
+
+stock_symbols = csv_read.iloc[:,0]
+
+stock_symbol_list = stock_symbols.values.tolist()
+print(stock_symbol_list)
