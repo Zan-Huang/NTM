17fb731cdf5ead76d436c2e456726487499341d0
zankner <zackankner@gmail.com>
2019-02-13 23:31:35 -0500

    begin train

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ import model
 
 sys.path.insert(0, 'NTMCode')
 
-import function_tests
-import controller
+def train():
+
+
+
+	with tf.Session() as sees:
+		saver = tf.train.Saver(tf.global_variables())
+		tf.global_variables_initializer().run()
+		train_writer = tf.summary.FileWriter(args.tensorboard, sens.graph)
+		for i in range(args.num_epocs):
+			feed_dict = {input:__need numpy array here__}
+
+		sess.run
 
 
 def main():
