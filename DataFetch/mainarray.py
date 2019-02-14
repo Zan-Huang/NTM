4aae731ebd42ef28027bfe97c233ae0e082845e9
Zan Huang <zan@zans-mbp.milton.edu>
2019-02-14 10:58:55 -0500

    Fetching Data Pains

--- DataFetch/mainarray.py
+++ DataFetch/mainarray.py
@@ -5,6 +5,7 @@ import pandas as pd
 from datetime import timedelta, date
 from tqdm import tqdm
 import pickle
+import time
 
 csv_file = "companylist.csv"
 
@@ -24,12 +25,12 @@ for i in tqdm(range(len(stock_symbol_list))):
     iter_parser = fetch.parse_function(stock_symbol_list[i], 'J6VF09SPJX6ORROI', date(2003, 2, 7), date(2019, 2, 8))
     download_stage = iter_parser.json_download()
     collect_stage = iter_parser.json_collect()
-    print(i)
     if not collect_stage:
         print("Null List detected %s" % stock_symbol_list[i])
-        throw("ERROR!")
+        throw("Null List detected %s" % stock_symbol_list[i])
 
     data_list.append(collect_stage)
+    time.sleep(1.75)
 
 print(data_list)
 
