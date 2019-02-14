713fca049fe56ee11df3f534a6011f089aa7e7c5
Zan Huang <zan@zans-mbp.milton.edu>
2019-02-13 23:26:02 -0500

    Finished data list processing. Now in need of better API Key. Should be available tomorrow

--- DataFetch/mainarray.py
+++ DataFetch/mainarray.py
@@ -2,13 +2,24 @@ import fetch
 import numpy as np
 import json
 import pandas as pd
+from datetime import timedelta, date
 
 csv_file = "companylist.csv"
 
 csv_read = pd.read_csv(csv_file)
-print(csv_read)
+#print(csv_read)
 
 stock_symbols = csv_read.iloc[:,0]
 
 stock_symbol_list = stock_symbols.values.tolist()
-print(stock_symbol_list)
+#print(stock_symbol_list)
+
+data_list = []
+
+for i in range(len(stock_symbol_list)):
+    iter_parser = fetch.parse_function(stock_symbol_list[i], 'J1X3N3PAL24DFCO4', date(2003, 2, 7), date(2019, 2, 8))
+    download_stage = iter_parser.json_download()
+    collect_stage = iter_parser.json_collect()
+    data_list.append(collect_stage)
+
+print(data_list)
