7e71242f3944f396a98e1966ddc74d8b0c3f83b2
Zan Huang <zan@zans-mbp.milton.edu>
2019-02-18 23:43:03 -0500

    still a few errors

--- DataFetch/mainarray.py
+++ DataFetch/mainarray.py
@@ -22,13 +22,17 @@ data_list = []
 print(len(stock_symbol_list))
 
 for i in tqdm(range(len(stock_symbol_list))):
-    iter_parser = fetch.parse_function(stock_symbol_list[i], 'J6VF09SPJX6ORROI', date(2018, 2, 2), date(2018, 2, 9))
+    iter_parser = fetch.parse_function(stock_symbol_list[i], 'J6VF09SPJX6ORROI', date(2016, 2, 2), date(2018, 2, 9))
     download_stage = iter_parser.json_download()
     collect_stage = iter_parser.json_collect()
+    for elements in collect_stage:
+        if not elements:
+            raise ValueError("null missing param")
     if not collect_stage:
-        throw("Null List detected %s" % stock_symbol_list[i])
+        raise ValueError("Null List detected %s" % stock_symbol_list[i])
 
     data_list.append(collect_stage)
+    time.sleep(2.1)
 
 
 numpy_data = np.asarray(data_list)
