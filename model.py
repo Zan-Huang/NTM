81686d82eae9b23a7c58a9d69ca6c7bfcf146b48
Zan Huang <zan@zans-mbp.milton.edu>
2019-02-22 13:29:10 -0500

    fixes

--- model.py
+++ model.py
@@ -13,6 +13,7 @@ class StockPredictor(object):
         self.x = tf.placeholder(name='x',dtype=tf.float32,shape=[batch_size, seq_length, vector_dim])
         self.y = label
 
+        cell = controller.NTM(512, memory_vector, x.shape, g)
         controller_state, read_vector, w = cell.initial_state(batch_size, tf.float32)
         for i in range(seq_length):
             output, controller_state, read_vector, w = cell.call(self.x[:,i,:],controller_state,w,read_vector)
