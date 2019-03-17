1cec21b2305062c02bb547bd16368340e0f8440c
ankner <zackankner@gmail.com>
2019-03-16 23:19:18 -0700

    fixed oob

--- NTMCode/content_focus.py
+++ NTMCode/content_focus.py
@@ -5,8 +5,9 @@ import numpy as np
 
 def similarity_measure(key_vector, memory_vector_slice):
     print("test")
-    print(key_vector.shape.as_list()[0])
-    print(memory_vector_slice.shape)
+    #print(key_vector.shape.as_list(),'key_vector')
+    #print(key_vector.shape.as_list()[0],'test')
+    print(memory_vector_slice.shape.as_list(), 'memory_vecotr')
     if key_vector.shape.as_list()[0] != memory_vector_slice.shape.as_list()[0]:
         raise Exception('The length of key vector is not equal to memory vector row length.')
     dot_product_term = tf.tensordot(key_vector, memory_vector_slice, 1)
@@ -24,11 +25,12 @@ def content_address(beta_strength, key_vector, memory_vector):
 
     content_bottom = 0
     for j in range(0, memory_vector.shape.as_list()[1]):
-        content_bot_temp = tf.math.exp(tf.multiply(beta_strength, similarity_measure(key_vector, tf.gather_nd(memory_vector,[1,j]))))
+        print(tf.gather_nd(memory_vector,[[1,j]]).shape.as_list(),'gather') 
+        content_bot_temp = tf.math.exp(tf.multiply(beta_strength, similarity_measure(key_vector, tf.gather_nd(memory_vector,[[1,j]]))))
         content_bottom = tf.add(content_bot_temp, content_bottom)
 
     for i in range(0, memory_vector.shape.as_list()[1]):
-        content_vector_top = tf.math.exp(tf.multiply(beta_strength, similarity_measure(key_vector, tf.gather_nd(memory_vector,[1,i]))))
+        content_vector_top = tf.math.exp(tf.multiply(beta_strength, similarity_measure(key_vector, tf.gather_nd(memory_vector,[[1,i]]))))
         final_top = content_vector_top / content_bottom
         composite_top.append(final_top)
     final_focus_vector = tf.stack([piece for piece in composite_top])
